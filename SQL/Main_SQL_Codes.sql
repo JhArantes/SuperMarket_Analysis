a33ffb2a01c169c87cb968359e43c6416074533e
JhArantesV <joaohenriqueavieira@gmail.com>
2024-07-16 13:33:59 -0300

    Improving the analysis

--- SQL/Main_SQL_Codes.sql
+++ SQL/Main_SQL_Codes.sql
@@ -1,2 +1,17 @@
 
+-- View the DataBase Columns
 
+SELECT * FROM Main_Table
+
+
+-- Requests for Work
+
+SELECT city, gender, total, time FROM Main_Table
+
+SELECT branch, total FROM Main_Table
+
+SELECT DISTINCT Branch, City FROM Main_Table
+
+SELECT branch, SUM(total) as total_sum
+FROM Main_Table
+GROUP BY branch;
